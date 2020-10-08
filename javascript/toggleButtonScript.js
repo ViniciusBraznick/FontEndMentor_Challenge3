9fd4aec6354b4bd075558bfce6d51dd04247758c
Vinicius Braznick <viniciusbraznick@gmail.com>
2020-10-07 23:59:54 -0300

    refactored JS code

--- javascript/toggleButtonScript.js
+++ javascript/toggleButtonScript.js
@@ -1,19 +1,10 @@
-let toggleBtn = document.querySelector('input')
-
-toggleBtn.addEventListener("click", () =>{
-    var rootColor = document.documentElement.style;
-    if (toggleBtn.value == 0) {
-    rootColor.setProperty('--color-bg-body', 'hsl(0, 0%, 100%)',);
-    rootColor.setProperty('--color-letters', 'hsl(230, 17%, 14%)');
-    rootColor.setProperty('--color-square-bg', 'hsl(225, 100%, 98%)');
-    rootColor.setProperty('--color-standardBox', 'hsl(227, 47%, 96%)');
-    rootColor.setProperty('--color-hover', 'hsl(217, 10%, 84%)');
-    toggleBtn.value = 1
+const btnToggle = document.querySelector('input')
+btnToggle.addEventListener('click', ()=>{
+    if(btnToggle.name == 'light'){
+        document.documentElement.setAttribute('theme', 'light')
+        btnToggle.name = 'dark'
     } else{
-    toggleBtn.value = 0
-    rootColor.setProperty('--color-bg-body', 'hsl(230, 17%, 14%)');
-    rootColor.setProperty('--color-letters', 'hsl(0, 0%, 100%)');
-    rootColor.setProperty('--color-square-bg', 'hsl(232, 19%, 15%)');
-    rootColor.setProperty('--color-standardBox', 'hsl(228, 28%, 20%)');
-    rootColor.setProperty('--color-hover', 'hsl(228, 30%, 25%)');}   
+        document.documentElement.setAttribute('theme', 'dark') 
+        btnToggle.name = 'light'
+    }
 })
