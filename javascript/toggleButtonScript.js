1cca1bb91488dc5b1bda1bf5cc786561e5a1495f
Vinicius Braznick <viniciusbraznick@gmail.com>
2021-04-07 15:37:18 -0300

    refactored the dark mode code

--- javascript/toggleButtonScript.js
+++ javascript/toggleButtonScript.js
@@ -1,19 +1,14 @@
-let toggleBtn = document.querySelector('input')
+const toggle_btn = document.querySelector('input');
 
-toggleBtn.addEventListener("click", () =>{
-    var rootColor = document.documentElement.style;
-    if (toggleBtn.value == 0) {
-    rootColor.setProperty('--color-bg-body', 'hsl(0, 0%, 100%)',);
-    rootColor.setProperty('--color-letters', 'hsl(230, 17%, 14%)');
-    rootColor.setProperty('--color-square-bg', 'hsl(225, 100%, 98%)');
-    rootColor.setProperty('--color-standardBox', 'hsl(227, 47%, 96%)');
-    rootColor.setProperty('--color-hover', 'hsl(217, 10%, 84%)');
-    toggleBtn.value = 1
-    } else{
-    toggleBtn.value = 0
-    rootColor.setProperty('--color-bg-body', 'hsl(230, 17%, 14%)');
-    rootColor.setProperty('--color-letters', 'hsl(0, 0%, 100%)');
-    rootColor.setProperty('--color-square-bg', 'hsl(232, 19%, 15%)');
-    rootColor.setProperty('--color-standardBox', 'hsl(228, 28%, 20%)');
-    rootColor.setProperty('--color-hover', 'hsl(228, 30%, 25%)');}   
+toggle_btn.addEventListener("click", () =>{
+    localStorage.getItem("theme") != 'dark' ? setTheme("dark") : setTheme("light")
 })
+
+const setTheme = (theme) => {
+    localStorage.setItem('theme', theme); 
+    document.body.setAttribute("data-theme", localStorage.getItem("theme"));
+    localStorage.getItem("theme") != 'dark' ? toggle_btn.classList.add("checked") : toggle_btn.classList.remove("checked")
+}
+
+const current_theme = localStorage.getItem("theme");
+current_theme && setTheme(current_theme);
